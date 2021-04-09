a8888412290794650ce7abd0d8cc67e478261018
odyssic <stevenbuddenjr@gmail.com>
2021-04-09 16:08:39 -0400

    ol as separate js

---
+++ main.js
@@ -0,0 +1,80 @@
+window.onload = init;
+
+function init() {
+    // Controls
+    const fullScreenControl = new ol.control.FullScreen();
+    const mousePositionControl = new ol.control.MousePosition();
+    const overViewMapControl = new ol.control.OverviewMap({
+        collapsed: false,
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM(),
+            }),
+        ],
+    });
+    const scaleLineControl = new ol.control.ScaleLine();
+    const zoomSliderControl = new ol.control.ZoomSlider();
+    const zoomToExtentControl = new ol.control.ZoomToExtent();
+
+    const map = new ol.Map({
+        view: new ol.View({
+            center: [-12080385, 7567433],
+            zoom: 3,
+            maxZoom: 6,
+            minZoom: 2,
+            rotation: 0,
+        }),
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM(),
+            }),
+        ],
+        target: "js-map",
+        keyboardEventTarget: document,
+        controls: ol.control
+            .defaults()
+            .extend([
+                fullScreenControl,
+                mousePositionControl,
+                overViewMapControl,
+                scaleLineControl,
+                zoomSliderControl,
+                zoomToExtentControl,
+            ]),
+    });
+
+    const popupContainerElement = document.getElementById("popup-coordinates");
+    const popup = new ol.Overlay({
+        element: popupContainerElement,
+        positioning: "top-right",
+    });
+
+    map.addOverlay(popup);
+
+    map.on("click", function(e) {
+        const clickedCoordinate = e.coordinate;
+        popup.setPosition(undefined);
+        popup.setPosition(clickedCoordinate);
+        popupContainerElement.innerHTML = clickedCoordinate;
+    });
+
+    // DragRotate Interaction
+    const dragRotateInteraction = new ol.interaction.DragRotate({
+        condition: ol.events.condition.altKeyOnly,
+    });
+
+    map.addInteraction(dragRotateInteraction);
+
+    // Draw Interaction
+    const drawInteraction = new ol.interaction.Draw({
+        type: "Polygon",
+        freehand: true,
+    });
+    map.addInteraction(drawInteraction);
+
+    drawInteraction.on("drawend", function(e) {
+        let parser = new ol.format.GeoJSON();
+        let drawnFeatures = parser.writeFeatures([e.feature]);
+        console.log(drawnFeatures);
+    });
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { OlMapComponent } from './components/ol-map/ol-map.component';
 import { MapComponent } from './components/map/map.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AddPropertyComponent } from './components/properties/add-property/add-property.component';
@@ -16,16 +15,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PropertyListComponent } from './components/properties/property-list/property-list.component';
 import { PropertyThumbnailComponent } from './components/properties/property-thumbnail/property-thumbnail.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FavoritePropertiesComponent } from './components/dashboard/favorite-properties/favorite-properties.component';
-import { IconComponent } from './components/icon/icon.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AgmCoreModule } from '@agm/core';
 import { ReportComponent } from './components/report/report.component';
 // import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 
-import { ToastrModule } from 'ngx-toastr';
+// import { ToastrModule } from 'ngx-toastr';
 // import { ToastrNotificationsComponent } from './components/toastr-notifications/toastr-notifications.component';
 
 @NgModule({
@@ -36,14 +32,11 @@ import { ToastrModule } from 'ngx-toastr';
     PropertyListComponent,
     PropertyThumbnailComponent,
     PropertyDetailComponent,
-    DashboardComponent,
-    FavoritePropertiesComponent,
     MapComponent,
     SearchPropertiesComponent,
-    IconComponent,
     HeaderComponent,
     ReportComponent,
-    OlMapComponent,
+
     // GoogleplacesComponent,
   ],
   imports: [
@@ -52,13 +45,13 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     AgmCoreModule.forRoot({
       // google map
-      apiKey: 'AIzaSyCC4FcGmIBqr91axvbKw1dzIo1nA8GoeMw',
+      apiKey: 'AIzaSyCNaS5t8NFDeburFa-AebErmmuvkDNxClQ',
     }),
-    // GooglePlaceModule,
+
     MyMaterialModule,
     HttpClientModule,
     ReactiveFormsModule,
-    ToastrModule.forRoot(),
+
     // ToastrNotificationsComponent,
   ],
   providers: [],
