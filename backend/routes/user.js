b61d86728b82a0a4be1d86bdd8cbe4ce45f2d663
odyssic <stevenbuddenjr@gmail.com>
2021-04-12 16:57:36 -0400

    auth service created

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const bcrypt = require("bcrypt")
+
+const User = express.Router('../models/user')
 
 const router = express.Router();
 
+router.property("/signup", (req, res, next) => {
+    bcrypt.hash(req.body.password, 10).then(hash => {
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        })
+        user.save().then(result => {
+            res.status(201).json({
+                message: 'User Created',
+                result: result
+            }).catch(err => {
+                res.status(500).json({
+                    error: err
+                })
+            })
+        })
+    })
 
+})
 
-
-
-module.exports = router
+module.exports = router;
