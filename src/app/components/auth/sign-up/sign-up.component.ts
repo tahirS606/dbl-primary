6b31890202077c97bcffe50a210349d79adae291
odyssic <stevenbuddenjr@gmail.com>
2021-04-13 19:23:44 -0400

    rerout not working now

--- src/app/components/auth/sign-up/sign-up.component.ts
+++ src/app/components/auth/sign-up/sign-up.component.ts
@@ -9,14 +9,15 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  isLoading = false;
-
+  
 
   onSignup(form: NgForm) {
     if (form.invalid) {
       return
     }
+  
     this.authService.createUser(form.value.email, form.value.password)
+    
   }
 
   constructor(public authService: AuthService ) { }
