16f1c6d33c906cdc121e3e72f35d0b142245cf21
odyssic <stevenbuddenjr@gmail.com>
2021-05-12 11:13:02 -0400

    map merged into report

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,7 +1,13 @@
+import { ReportService } from './../services/report.service';
+import { Task } from './../../models/task.model';
 import { ActivatedRoute } from '@angular/router';
 import { PropertyService } from './../../shared/property.service';
 import { Property } from './../../models/property.model';
 import { Component, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder,  
+  FormGroup,
+  FormArray,
+  FormControl,} from '@angular/forms';
 
 @Component({
   selector: 'app-report',
@@ -10,17 +16,56 @@ import { Component, Input, OnInit, Output } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  @Output() property!: Property; 
+  tasks: Task[] = []
+  form!: FormGroup;
+
+
+  webData = [
+    { id: 1, name: 'Raking' },
+    { id: 2, name: 'Mowing' },
+    { id: 3, name: 'Weeding' },
+    { id: 4, name: 'Blowing' },
+    { id: 5, name: 'Trimming' }
+  ];
+
+  get ordersFormArray() {
+    return this.form.controls.orders as FormArray;
+  }
+
+  property!: Property; 
   
   propertyId!: any; 
 
   date = new Date()
 
   constructor(private propertyService: PropertyService,
-    private route: ActivatedRoute
-    ) { }
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    public reportService: ReportService,
+    ) { 
+      this.form = this.formBuilder.group({
+        orders: new FormArray([])
+      });
+      this.addCheckboxesToForm();
+    }
+
+    private addCheckboxesToForm() {
+      this.webData.forEach(() => this.ordersFormArray.push(new FormControl(false)));
+    }
+
+    submit() {
+      const selectedOrderIds = this.form.value.orders
+        .map((checked:Boolean, i:number) => checked ? this.webData[i].id : null)
+        // .filter(v => v !== null);
+      console.log(selectedOrderIds);
+    }
 
   ngOnInit(): void {
+
+    const fetchedTasks = this.reportService.getTasks().subscribe()
+
+    console.log('fetchedTasks', fetchedTasks)
+
     this.propertyId = this.route.snapshot.paramMap.get('propertyId');
     
         this.propertyService
