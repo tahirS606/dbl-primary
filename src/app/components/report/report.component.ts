e0c3d8fe71610d9605ee13d55400aa1c74fd7655
odyssic <stevenbuddenjr@gmail.com>
2021-06-08 20:44:01 -0400

    try a NEW APPROACH be not afraid

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -73,6 +73,130 @@ export class ReportComponent implements OnInit {
   addTasksButtonDisabled: boolean = true;
   taskCheckboxButtonDisabled: boolean = true;
 
+  initDrawingManager(map:any) {
+
+    const strokeColors = {
+      red: 'red',
+      blue: '#99EEFF', 
+      green: '#008080',
+      yellow: 'yellow',
+      lavender: "#C5C5FF",
+    }
+
+    // plan: will 'randomly' cycle through a list of 16 or so colors (enough to never run out) upon new polygon creation
+
+    let areaObjects : [{}] = [{}];
+    areaObjects.shift()
+    
+    const options = {
+      drawingMode: google.maps.drawing.OverlayType.POLYGON,
+      drawingControl: true,
+      drawingControlOptions: {
+        drawingModes: ["polygon"]
+      },
+
+      polygonOptions: {
+        outline: true, 
+        draggable: true,
+        editable: true,
+        fillColor: "#ffff00",
+        fillOpacity: .25,
+        strokeWeight: 5,
+        strokeColor: '#99EEFF',
+        clickable: true,
+        zIndex: 1,
+        fullScreenControl: true, 
+      },
+    
+    };
+
+    const drawingManager = new google.maps.drawing.DrawingManager(options);
+    
+    drawingManager.setMap(map);
+    
+    let number = 0;
+    
+    this.polygonComplete = false
+    const _self = this; 
+
+    google.maps.event.addListener(drawingManager, 'polygoncomplete', function (polygon:any) {
+
+      _self.polygonComplete  = true; 
+      _self.collectionCount
+      console.log('collection count', _self.collectionCount)
+      
+
+      const len = polygon.getPath().getLength();
+      const polyArrayLatLng = [];
+      
+      for (let i = 0; i < len; i++) {
+        const vertex = polygon.getPath().getAt(i);
+        const vertexLatLng = {lat: vertex.lat(), lng: vertex.lng()};
+        polyArrayLatLng.push(vertexLatLng);
+      }
+  
+      // the last point of polygon should be always the same as the first point
+      polyArrayLatLng.push(polyArrayLatLng[0]);
+
+      number = number+1 
+      let polygonName = 'Area ' + number    
+      let areaArray = {name: '', area: [] }
+      const areaObject = Object.create(areaArray)
+      areaObject.area.shift()
+      areaObject.name = polygonName
+      areaObject.area = polyArrayLatLng
+      areaObjects.push(areaObject)
+      
+    });
+
+  }
+
+  addTaskstoArea() {
+
+    this.areasWithTasksLength = 0
+   
+    const allTasks = this.form.value.tasks.map((checked:Boolean, i:number) => (checked) ? this.webData[i].name
+      : null);
+    
+      // filters out null objects
+      allTasks.map((task:any)=>{
+        if (task !== null){
+          this.tasks.push(task)
+        }
+      })
+
+      let areasWithTasks = {name: '', areas: [{}], tasks: [{}]}
+      console.log('areas for report before adding tasks', this.areasForReport)
+
+      areasWithTasks.areas.push(this.areasForReport)
+      // this.areasForReport = []
+
+      console.log('area added to areasWithTasks', areasWithTasks)
+
+      areasWithTasks.tasks.push(this.tasks)
+
+      this.areasWithTasksLength = areasWithTasks.tasks.length
+
+      console.log('after tasks added areasWithTasks', areasWithTasks)
+
+    // working, now except areas keep stacking, even when new tasks added
+
+      this.collectionForReport.push(areasWithTasks)
+      this.collectionCount = this.collectionCount + 1
+
+      let collectionName = 'Collection ' + this.collectionCount.toString()
+      areasWithTasks.name = collectionName
+      console.log('areasWithTasks after tasks added', areasWithTasks)
+
+      // works now, except areas keep adding up. !!!
+      console.log('collectionForReport - Should be more than one', this.collectionForReport)
+
+      // so first will have a b, second will have abc, third will have abcd. 
+
+      this.form.reset()
+      
+      }
+
   webData = [
     { id: 1, name: 'Raking' },
     { id: 2, name: 'Mowing' },
@@ -118,51 +242,7 @@ export class ReportComponent implements OnInit {
 
   // end clear map (doesn't work yet)
 
-    addTaskstoArea() {
-
-      this.areasWithTasksLength = 0
-     
-      const allTasks = this.form.value.tasks.map((checked:Boolean, i:number) => (checked) ? this.webData[i].name
-        : null);
-      
-        // filters out null objects
-        allTasks.map((task:any)=>{
-          if (task !== null){
-            this.tasks.push(task)
-          }
-        })
-
-        let areasWithTasks = {name: '', areas: [{}], tasks: [{}]}
-        console.log('areas for report before adding tasks', this.areasForReport)
-
-        areasWithTasks.areas.push(this.areasForReport)
-        // this.areasForReport = []
-
-        console.log('area added to areasWithTasks', areasWithTasks)
-
-        areasWithTasks.tasks.push(this.tasks)
-
-        this.areasWithTasksLength = areasWithTasks.tasks.length
-
-        console.log('after tasks added areasWithTasks', areasWithTasks)
-
-      // working, now except areas keep stacking, even when new tasks added
-
-        this.collectionForReport.push(areasWithTasks)
-        this.collectionCount = this.collectionCount + 1
-
-        let collectionName = 'Collection ' + this.collectionCount.toString()
-        areasWithTasks.name = collectionName
-        console.log('areasWithTasks after tasks added', areasWithTasks)
-
-        // works now, except areas keep adding up. !!!
-        console.log('collectionForReport - Should be more than one', this.collectionForReport)
-
-        // so first will have a b, second will have abc, third will have abcd. 
-
-        this.form.reset()
-        
-        }
+    
 
     saveReport(){
     
@@ -209,94 +289,5 @@ export class ReportComponent implements OnInit {
     this.initDrawingManager(map);
   }
 
-  initDrawingManager(map:any) {
-
-    const strokeColors = {
-      red: 'red',
-      blue: '#99EEFF', 
-      green: '#008080',
-      yellow: 'yellow',
-      lavender: "#C5C5FF",
-    }
-
-    // plan: will 'randomly' cycle through a list of 16 or so colors (enough to never run out) upon new polygon creation
-
-    let areaObjects : [{}] = [{}];
-    areaObjects.shift()
-    
-    const options = {
-      drawingMode: google.maps.drawing.OverlayType.POLYGON,
-      drawingControl: true,
-      drawingControlOptions: {
-        drawingModes: ["polygon"]
-      },
-
-      polygonOptions: {
-        outline: true, 
-        draggable: true,
-        editable: true,
-        fillColor: "#ffff00",
-        fillOpacity: .25,
-        strokeWeight: 5,
-        strokeColor: '#99EEFF',
-        clickable: true,
-        zIndex: 1,
-        fullScreenControl: true, 
-      },
-    
-    };
-
-    const drawingManager = new google.maps.drawing.DrawingManager(options);
-    
-    drawingManager.setMap(map);
-    
-    let number = 0;
-
-    // how to access .this within event below. 
-    
-    // this.x = 0;
-    // this.y = 0;
-    // var _self = this;
-
-    // _self.x = event.pageX;     // Is now able to access Map's member variable "x"
-    //     _self.y = event.pageY; 
-    
-    this.polygonComplete = false
-    const _self = this; 
-
-    google.maps.event.addListener(drawingManager, 'polygoncomplete', function (polygon:any) {
-
-      _self.polygonComplete  = true; 
-      _self.collectionCount
-      console.log('collection count', _self.collectionCount)
-      
-
-      const len = polygon.getPath().getLength();
-      const polyArrayLatLng = [];
-      
-      for (let i = 0; i < len; i++) {
-        const vertex = polygon.getPath().getAt(i);
-        const vertexLatLng = {lat: vertex.lat(), lng: vertex.lng()};
-        polyArrayLatLng.push(vertexLatLng);
-      }
   
-      // the last point of polygon should be always the same as the first point
-      polyArrayLatLng.push(polyArrayLatLng[0]);
-
-      number = number+1 
-      let polygonName = 'Area ' + number    
-      let areaArray = {name: '', area: [] }
-      const areaObject = Object.create(areaArray)
-      areaObject.area.shift()
-      areaObject.name = polygonName
-      areaObject.area = polyArrayLatLng
-      console.log('area object', areaObject)  
-      areaObjects.push(areaObject)
-      console.log('array of area objects after push of area Object', areaObjects)
-    });
-
-    this.areaObjectsGlobal.push(areaObjects)
-    console.log('making global collections for report', this.areaObjectsGlobal)
-
-  }
 }
